9447d1ebc42572373f62230167be235c9fd3ad9f
b1a0k <blank@blank.ren>
2018-07-17 09:57:05 +0800

    订单页数据接口

--- src/com/shopping/controller/OrderController.java
+++ src/com/shopping/controller/OrderController.java
@@ -1,9 +1,41 @@
 package com.shopping.controller;
 
+import java.math.BigInteger;
+import java.util.Optional;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.shopping.entity.Order;
+import com.shopping.entity.User;
+import com.shopping.service.OrderService;
+import com.shopping.util.HttpVal;
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
 @Controller
 @RequestMapping("/order")
 public class OrderController {
+	
+	@Resource
+	OrderService oService;
+	
+	@RequestMapping
+	public String order(HttpSession session, Model model) {
+		User u = new User();
+		u.setUserId(new BigInteger("1"));
+		session.setAttribute(HttpVal.SESSION_COMMON_USER_KEY,u);
+		Optional<User> userOpt = Optional.ofNullable((User)session.getAttribute(HttpVal.SESSION_COMMON_USER_KEY));
+//		if (!userOpt.isPresent()) {
+//			return "loginPage";
+//		}
+		userOpt.ifPresent(user ->{
+			model.addAttribute("orders", oService.findOrderByUserId(user.getUserId()));
+		});
+		return "bought";
+	}
 	
 }
