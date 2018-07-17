3039526bc1ce1578838560b6043a556bfb5b0b1f
b1a0k <blank@blank.ren>
2018-07-17 09:59:17 +0800

    订单页面功能实现

--- src/com/shopping/service/OrderService.java
+++ src/com/shopping/service/OrderService.java
@@ -1,4 +1,11 @@
 package com.shopping.service;
 
+import java.math.BigInteger;
+import java.util.List;
+
+import com.shopping.entity.Order;
+
 public interface OrderService {
+	
+	List<Order> findOrderByUserId(BigInteger id);
 }
--- src/com/shopping/service/impl/OrderServiceImpl.java
+++ src/com/shopping/service/impl/OrderServiceImpl.java
@@ -1,14 +1,29 @@
 package com.shopping.service.impl;
 
+import java.math.BigInteger;
+import java.util.List;
+import java.util.Objects;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
 import com.shopping.dao.OrderDao;
+import com.shopping.entity.Order;
 import com.shopping.service.OrderService;
 
 @Service
 public class OrderServiceImpl implements OrderService {
 	@Resource
 	private OrderDao oDao;
+
+	@Override
+	public List<Order> findOrderByUserId(BigInteger id) {
+		if (Objects.isNull(id)) {
+			return null;
+		}
+		return oDao.findOrderByUserId(id);
+	}
+	
+	
 }
