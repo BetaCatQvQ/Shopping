0863fb0bd030eac998f9ba8610f6f5a303211ed7
b1a0k <blank@blank.ren>
2018-07-17 12:21:22 +0800

    添加初始化监听器

---
+++ src/com/shopping/listener/InitListener.java
@@ -0,0 +1,20 @@
+package com.shopping.listener;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class InitListener implements ServletContextListener{
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		ServletContext context = arg0.getServletContext();
+		context.setAttribute("ctx", context.getContextPath());
+		System.out.println(context.getAttribute("ctx"));
+	}
+
+}
