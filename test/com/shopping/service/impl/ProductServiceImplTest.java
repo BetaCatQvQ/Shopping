dcfc5c2287a63c5048c79edd4322ca906fb39c05
b1a0k <blank@blank.ren>
2018-07-24 10:06:29 +0800

    合并修改

--- test/com/shopping/service/impl/ProductServiceImplTest.java
+++ test/com/shopping/service/impl/ProductServiceImplTest.java
@@ -1,8 +1,20 @@
 package com.shopping.service.impl;
 
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.shopping.entity.Page;
 import com.shopping.service.ProductService;
 import com.shopping.util.SpringTool;
 
 public class ProductServiceImplTest {
 	private ProductService userService = SpringTool.getBean(ProductServiceImpl.class);
+
+	@Test
+	public void getProductListByCategoryThreeIdTest() {
+		Page<Map<String, Object>> page = new Page<>(1);
+		page = userService.getProductListByCategoryThreeId(1, page);
+		System.out.println("-----------testList-----------");
+	}
 }
